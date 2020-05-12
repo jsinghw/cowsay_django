f6937cbf07b9e0806be4379185fae19120d61e95
Jaspal Singh <jsinghw94@gmail.com>
2020-05-12 16:08:31 -0400

    Finished

--- app/views.py
+++ app/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render
 
 from app.models import CowsayModel
+from app.forms import CowsayForm
+
+from subprocess import run
 
 
-# Create your views here.
 def index(request):
-    # data = CowsayModel.objects.all()
-    return render(request, 'index.html')
+    html = "index.html"
+
+    if request.method == "POST":
+        form = CowsayForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            CowsayModel.objects.create(
+                text=data['text']
+            )
+            output = run(['cowsay', data["text"]], capture_output=True).stdout
+            output = output.decode('utf-8')
+    form = CowsayForm()
+    return render(request, html, {'form': form, 'output': output})
 
 
 def history_view(request):
-    return render(request, 'history.html')
+    data = []
+    cowOutput = CowsayModel.objects.all()
+    for i in cowOutput:
+        data.append(i)
+    data = data[-10:][::-1]
+    return render(request,
+                  'history.html',
+                  {'data': data}
+                  )
